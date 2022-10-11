fd0fe7d4463b8ffded6f1700112b5dc9d778f12c
alfina.megasiwi <alfina.megasiwi@ui.ac.id>
2022-10-11 16:21:41 +0700

    data monitoring controller

--- app/Http/Controllers/DataMonitoringController.php
+++ app/Http/Controllers/DataMonitoringController.php
@@ -6,11 +6,11 @@ class DataMonitoringController extends Controller
 {
     public $data = [];
 
-    public function __construct(){
+    public function __construct()
+    {
         $db = new DatabaseFirebase();
         $this->data = $db->index();
     }
-
 
     // Mengambil data untuk hari ini
     public function todaystat()
@@ -23,23 +23,37 @@ class DataMonitoringController extends Controller
     public function getdata($date1, $date2)
     {
         $dates = $this->getBetweenDates($date1,  $date2);
+        $date_arr = array();
         $data_arr = array();
+        $time_arr = array();
+        $error_arr = array();
+
         for ($x = 0; $x < count($dates); $x++) {
-            array_push($data_arr, $this->data[$dates[$x]]);
+            array_push($date_arr, $this->data[$dates[$x]]['DATE']);
+            array_push($data_arr, (int)$this->data[$dates[$x]]['DATA']);
+            array_push($time_arr, (int)$this->data[$dates[$x]]['TIME']);
+            array_push($error_arr, (int)$this->data[$dates[$x]]['ERROR']);
         }
 
-        return json_encode($data_arr);
+        return json_encode(array(
+            'data' => $data_arr,
+            'date' => $date_arr,
+            'time' => $time_arr,
+            'error' => $error_arr
+        ));
     }
 
     // Mengambil data minggu ini
-    public function thisweekdata(){
+    public function thisweekdata()
+    {
         $this_week_monday = date('d-m-Y', strtotime('this week monday'));
         $today = date('d-m-Y', strtotime('today -1 day'));
         return $this->getdata($this_week_monday, $today);
     }
 
     // Mengmabil data mingguan berdasarkan tanggal yang diberikan
-    public function weeklydata($date){
+    public function weeklydata($date)
+    {
         $date = strtotime($date);
         $this_week_monday = date('d-m-Y', strtotime('this week monday', $date));
         $this_week_sunday = date('d-m-Y', strtotime('this week sunday', $date));
@@ -66,5 +80,4 @@ class DataMonitoringController extends Controller
 
         return $rangArray;
     }
-
 }
