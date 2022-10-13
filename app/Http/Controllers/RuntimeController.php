3e50815065ca9f669fc76ad610b7b6be216b9abb
alfina.megasiwi <alfina.megasiwi@ui.ac.id>
2022-10-13 10:56:58 +0700

    runtime data monthly and annual done

--- app/Http/Controllers/RuntimeController.php
+++ app/Http/Controllers/RuntimeController.php
@@ -11,31 +11,100 @@ class RuntimeController extends Controller
         $this->DatabaseFirebase = new DatabaseFirebase();
     }
 
-    public function thismonthruntime()
+    public function getdata($date1, $date2, $type)
     {
-        $this_month_first_day = date('d-m-Y', strtotime('first day of this month'));
-        $today = date('d-m-Y', strtotime('today -1 day'));
-        $dates = $this->DatabaseFirebase->getBetweenDates($this_month_first_day, $today);
-        $dates_chunk = array_chunk($dates, 7);
+        $dates = $this->DatabaseFirebase->getBetweenDates($date1, $date2);
+        $dates_chunk = [];
+        if ($type == "week" || $type == "month") {
+            $dates_chunk = array_chunk($dates, 7);
+        } else {
+            $years = array();
+            $months = array();
+            foreach ($dates as $d) {
+                list($day, $month, $year) = explode("-", $d);
+                $years[$year][] = $d;
+                $months[$year . "-" . $month][] = $d;
+            }
+            $dates_chunk = array_values($months);
+        }
 
         $accumulation_arr = [];
         for ($item_arr = 0; $item_arr < count($dates_chunk); $item_arr++) {
             $temp_arr = [];
             for ($item = 0; $item < count($dates_chunk[$item_arr]); $item++) {
-                array_push($temp_arr, $this->DatabaseFirebase->data[$dates_chunk[$item_arr][$item]]['RUNTIME']);
+                array_push($temp_arr, $this->DatabaseFirebase->data[$dates_chunk[$item_arr][$item]]['RUNTIME'] ?? 0);
             }
-            $average = number_format((float)array_sum($temp_arr)/count($temp_arr), 2, '.', '');
-            $accumulation_arr["Week" . $item_arr+1 ] = (float)$average ;
-
+            if ($type == "month" || $type == "year") {
+                $average = number_format((float)array_sum($temp_arr) / count($temp_arr), 2, '.', '');
+                array_push($accumulation_arr,  (float)$average);
+            } else {
+                array_push($accumulation_arr, $temp_arr);
+            }
         }
-        return json_encode($accumulation_arr);
-
+        return $accumulation_arr;
     }
 
-    public function monthlyruntime($date){
-        $date = strtotime($date);
-        // $first_day_of_the_month = date('d-m-Y', strtotime('first day of this month', $date));
-        // $last_day_of_the_month = date('d-m-Y', strtotime('last day of this month', $date));
-        // return $this->getdatagetdata($this_week_monday, $this_week_sunday);
+    public function thisweekruntime()
+    {
+        $this_week_monday = date('d-m-Y', strtotime('this week monday'));
+        $today = date('d-m-Y', strtotime('today -1 day'));
+        return json_encode($this->getdata($this_week_monday, $today, "week"));
+    }
+
+    public function weeklyruntime($date)
+    {
+        $first_day = strtotime('monday this week');
+        $last_day = strtotime('sunday this week');
+
+        if ((strtotime($date) > $first_day) && (strtotime($date) < $last_day)) {
+            return $this->thisweekruntime();
+        } else {
+            $date = strtotime($date);
+            $this_week_monday = date('d-m-Y', strtotime('this week monday', $date));
+            $this_week_sunday = date('d-m-Y', strtotime('this week sunday', $date));
+            return $this->getdata($this_week_monday, $this_week_sunday, "week");
+        }
+    }
+
+    public function thismonthruntime()
+    {
+        $this_month_first_day = date('d-m-Y', strtotime('first day of this month'));
+        $today = date('d-m-Y', strtotime('today -1 day'));
+        return json_encode($this->getdata($this_month_first_day, $today, "month"));
+    }
+
+    public function monthlyruntime($date)
+    {
+        $first_day = strtotime('first day of this month');
+        $last_day = strtotime('last day of this month');
+        if ((strtotime($date) > $first_day) && (strtotime($date) < $last_day)) {
+            return $this->thismonthruntime();
+        } else {
+            $date = strtotime($date);
+            $this_month_first_day = date('d-m-Y', strtotime('first day of this month', $date));
+            $this_month_last_day = date('d-m-Y', strtotime('last day of this month', $date));
+            return json_encode($this->getdata($this_month_first_day, $this_month_last_day, "month"));
+        }
+    }
+
+    public function thisyearruntime()
+    {
+        $this_year_first_day = date('d-m-Y', strtotime('first day of january this year'));
+        $today = date('d-m-Y', strtotime('today -1 day'));
+        return json_encode($this->getdata($this_year_first_day, $today, "year"));
+    }
+
+    public function annualruntime($date)
+    {
+        $first_day = strtotime('first day of january this year');
+        $last_day = strtotime('last day of december this year');
+        if ((strtotime($date) > $first_day) && (strtotime($date) < $last_day)) {
+            return $this->thisyearruntime();
+        } else {
+            $date = strtotime($date);
+            $this_year_first_day = date('d-m-Y', strtotime('first day of january this year', $date));
+            $this_year_last_day = date('d-m-Y', strtotime('llast day of december this year', $date));
+            return json_encode($this->getdata($this_year_first_day, $this_year_last_day, "year"));
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ use App\Http\Controllers\RuntimeController;
 Route::get('get-data/{date1}/{date2}', [DataMonitoringController::class, 'getdata'])->name('firebase.getdata');
 Route::get('today-stat', [DataMonitoringController::class, 'todaystat'])->name('firebase.todaystat');
 Route::get('this-week-data', [DataMonitoringController::class, 'thisweekdata'])->name('firebase.thisweekdata');
-Route::get('weekly-data/{date1}', [DataMonitoringController::class, 'weeklydata'])->name('firebase.weeklydata');
+Route::get('weekly-data/{date}', [DataMonitoringController::class, 'weeklydata'])->name('firebase.weeklydata');
 
+Route::get('this-week-runtime', [RuntimeController::class, 'thisweekruntime'])->name('firebase.thisweekruntime');
+Route::get('weeklyruntime/{date}', [RuntimeController::class, 'weeklyruntime'])->name('firebase.weeklyruntime');
 Route::get('this-month-runtime', [RuntimeController::class, 'thismonthruntime'])->name('firebase.thismonthruntime');
+Route::get('monthlyruntime/{date}', [RuntimeController::class, 'monthlyruntime'])->name('firebase.monthlyruntime');
+Route::get('this-year-runtime', [RuntimeController::class, 'thisyearruntime'])->name('firebase.thisyearruntime');
+Route::get('annualruntime/{date}', [RuntimeController::class, 'annualruntime'])->name('firebase.annualruntime');
