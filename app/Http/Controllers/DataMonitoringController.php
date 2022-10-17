c73d000620523c139201c9b3bb0e8b0cfc60c00d
bagas-09 <93715655+bagas-09@users.noreply.github.com>
2022-10-17 08:42:17 +0700

    bulanan chart

--- app/Http/Controllers/DataMonitoringController.php
+++ app/Http/Controllers/DataMonitoringController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use PhpParser\Node\Expr\Cast\Array_;
+
 class DataMonitoringController extends Controller
 {
     public $DatabaseFirebase = null;
@@ -64,6 +66,75 @@ class DataMonitoringController extends Controller
             $this_week_monday = date('d-m-Y', strtotime('this week monday', $date));
             $this_week_sunday = date('d-m-Y', strtotime('this week sunday', $date));
             return $this->getdata($this_week_monday, $this_week_sunday);
+        }
+    }
+
+    public function getdata2($date1, $date2, $type){
+        $dates =  $this->DatabaseFirebase->getBetweenDates($date1, $date2);
+        $dates_chunk = [];
+        if ($type == "week" || $type == "month"){
+            $dates_chunk = array_chunk($dates, 7);
+        } else {
+            $year = array();
+            $months =  array();
+            foreach ($dates as $d){
+                list($day, $month, $year) = explode("-", $d);
+                $years[$year][] = $d;
+                $months[$year . "-" . $month][] = $d;
+            }
+            $dates_chunk = array_values($months);
+        }
+
+        $accumulation_arr_data = [];
+        $accumulation_arr_time = [];
+        $accumulation_arr_error = [];
+        $accumulation_arr = [];
+        for ($item_arr = 0; $item_arr < count($dates_chunk); $item_arr++) {
+            $temp_arr_data = [];
+            $temp_arr_time = [];
+            $temp_arr_error = [];
+            for ($item = 0; $item < count($dates_chunk[$item_arr]); $item++) {
+                array_push($temp_arr_data, $this->DatabaseFirebase->data[$dates_chunk[$item_arr][$item]]['DATA'] ?? 0);
+                array_push($temp_arr_time, $this->DatabaseFirebase->data[$dates_chunk[$item_arr][$item]]['TIME'] ?? 0);
+                array_push($temp_arr_error, $this->DatabaseFirebase->data[$dates_chunk[$item_arr][$item]]['ERROR'] ?? 0);
+            }
+            if ($type == "month" || $type == "year") {
+                $average_data = number_format((float)array_sum($temp_arr_data) / count($temp_arr_data), 2, '.', '');
+                $average_time = number_format((float)array_sum($temp_arr_time) / count($temp_arr_time), 2, '.', '');
+                $average_error = number_format((float)array_sum($temp_arr_error) / count($temp_arr_error), 2, '.', '');
+                array_push($accumulation_arr_data,  (float)$average_data);
+                array_push($accumulation_arr_time,  (float)$average_time);
+                array_push($accumulation_arr_error,  (float)$average_error);
+            } else {
+                array_push($accumulation_arr_data, $temp_arr_data);
+                array_push($accumulation_arr_time, $temp_arr_time);
+                array_push($accumulation_arr_error, $temp_arr_error);
+            }
+        }
+        array_push($accumulation_arr, $accumulation_arr_data);
+        array_push($accumulation_arr, $accumulation_arr_time);
+        array_push($accumulation_arr, $accumulation_arr_error);
+        return  $accumulation_arr;
+    }
+
+    public function thismonthdata(){
+        $this_month_first_day = date('d-m-Y', strtotime('first day of this month'));
+        $today = date('d-m-Y', strtotime('today -1 day')); 
+        
+        return json_encode($this->getdata2($this_month_first_day, $today, "month"));
+    }
+
+    public function monthlydata($date)
+    {
+        $first_day = strtotime('first day of this month');
+        $last_day = strtotime('last day of this month');
+        if ((strtotime($date) > $first_day) && (strtotime($date) < $last_day)) {
+            return $this->thismonthdata();
+        } else {
+            $date = strtotime($date);
+            $this_month_first_day = date('d-m-Y', strtotime('first day of this month', $date));
+            $this_month_last_day = date('d-m-Y', strtotime('last day of this month', $date));
+            return json_encode($this->getdata2($this_month_first_day, $this_month_last_day, "month"));
         }
     }
 }
